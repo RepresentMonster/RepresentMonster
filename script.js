cc49ae6a3bf3846b508150cbb96fae1f09cccffb
RepresentMonster <68795477+RepresentMonster@users.noreply.github.com>
2025-07-05 23:03:53 +1000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function setActive(el) {
-  document.querySelectorAll('.nav-btn').forEach(btn => btn.classList.remove('active'));
-  el.classList.add('active');
-}
-
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-}
